95f656a04faeeb9b6ca90eee0debbd828084ee77
DESKTOP-G6VSS41\Truong Dung <truongdung0502@hotmail.com>
2018-06-12 00:09:23 -0500

    update lab 8

---
+++ project/coffeeShop/target/classes/static/js/main.js
@@ -0,0 +1,13 @@
+function myAlertTop(){
+  $(".myAlert-top").show();
+  setTimeout(function(){
+    $(".myAlert-top").hide(); 
+  }, 2000);
+}
+
+function myAlertBottom(){
+  $(".myAlert-bottom").show();
+  setTimeout(function(){
+    $(".myAlert-bottom").hide(); 
+  }, 2000);
+}
